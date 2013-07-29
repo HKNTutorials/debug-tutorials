b88bdb13a00e0f80c94d0ab06de49e8d953ca7ce
Tej Chajed <tchajed@palantir.com>
2013-07-28 18:56:47 -0700

    make exp simpler

--- exp/exp.c
+++ exp/exp.c
@@ -25,7 +25,7 @@ double exponential(double x) {
   double powx = 1.0;
   // 10 iterations should be enough since 10! is very large; too much beyond
   // that and dividing by a very large value will give unexpected results.
-  for (int degree = 0; degree < 3; degree++) {
+  for (int degree = 0; degree < 10; degree++) {
     accum += powx / (double) factorial(degree);
     powx *= x;
   }
