2cf857cc430040c27d5133c26fc9d81117c7a464
Tej Chajed <tchajed@palantir.com>
2013-07-28 22:14:18 -0700

    Add solution and extra tests to square_ints

--- square_ints/square_ints.c
+++ square_ints/square_ints.c
@@ -6,15 +6,14 @@
 
 int global_array[SIZE];
 int* getArray() {
-  // TODO: implement this function
-  return NULL;
+  return global_array;
 }
 
 // Fill array with the values startNum ... startNum + len - 1. Array should be
 // of size at least len, of which the first len elements are filled.
 void fillArray(int *array, int startNum, int len) {
   for (int i = 0; i < len; i++) {
-    array[startNum + i] = i;
+    array[i] = startNum + i;
   }
 }
 
@@ -23,6 +22,34 @@ void squareArray(int *array, int len) {
   for (int i = 0; i < len; i++) {
     array[i] = array[i]*array[i];
   }
+}
+
+// This test case passes, indicating startNum is being treated incorrectly
+void TestFillArrayFromZero(CuTest *tc) {
+  int *array = getArray();
+  fillArray(array, 0, SIZE);
+  CuAssertIntEquals(tc, 0, array[0]);
+  CuAssertIntEquals(tc, 3, array[3]);
+  CuAssertIntEquals(tc, SIZE - 1, array[SIZE - 1]);
+}
+
+// Before fixing the bug in fillArray, if you disabled TestSquareArray (to
+// avoid the segfault) you would find this test doesn't segfault but fails.
+void TestFillArrayFromSmallValue(CuTest *tc) {
+  int *array = getArray();
+  fillArray(array, 10, SIZE);
+  CuAssertIntEquals(tc, 10, array[0]);
+  CuAssertIntEquals(tc, 10 + SIZE - 1, array[SIZE - 1]);
+}
+
+// It's a good idea to try special values like 0
+void TestSquareArrayFromZero(CuTest *tc) {
+  int *array = getArray();
+  fillArray(array, 0, SIZE);
+  squareArray(array, SIZE);
+  CuAssertIntEquals(tc, 0, array[0]);
+  CuAssertIntEquals(tc, 10*10, array[10]);
+  CuAssertIntEquals(tc, (SIZE-1)*(SIZE-1), array[SIZE - 1]);
 }
 
 void TestSquareArray(CuTest *tc) {
