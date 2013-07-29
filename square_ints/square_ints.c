44bedeef3b9562da91d54ed0bb3473b2fdddc90d
Tej Chajed <tchajed@palantir.com>
2013-07-28 21:45:52 -0700

    Add basic port of square_ints
    - tricky logic (eg, iterating in reverse order) removed
    - added another segmentation fault (bus error on Mac OS X)

---
+++ square_ints/square_ints.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "CuTest.h"
+
+#define SIZE (50)
+
+int global_array[SIZE];
+int* getArray() {
+  // TODO: implement this function
+  return NULL;
+}
+
+// Fill array with the values startNum ... startNum + len - 1. Array should be
+// of size at least len, of which the first len elements are filled.
+void fillArray(int *array, int startNum, int len) {
+  for (int i = 0; i < len; i++) {
+    array[startNum + i] = i;
+  }
+}
+
+// Square each element of array assuming it is of size len
+void squareArray(int *array, int len) {
+  for (int i = 0; i < len; i++) {
+    array[i] = array[i]*array[i];
+  }
+}
+
+void TestSquareArray(CuTest *tc) {
+  int *array = getArray();
+  fillArray(array, 1000, SIZE);
+  squareArray(array, SIZE);
+  // These assertions have correct expected values
+  CuAssertIntEquals(tc, 1000*1000, array[0]);
+  CuAssertIntEquals(tc, 1026*1026, array[26]);
+  CuAssertIntEquals(tc, 1049 * 1049, array[49]);
+}
